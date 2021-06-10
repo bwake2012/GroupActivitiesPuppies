7863ffa52026f95450b449ea4a555f87c6ef5be7
Bob Wakefield <bob@cockleburr.net>
2021-06-10 14:51:23 -0500

    Initial Commit

---
+++ GroupActivitiesPuppies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupActivitiesPuppies
+//
+//  Created by Bob Wakefield on 6/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
